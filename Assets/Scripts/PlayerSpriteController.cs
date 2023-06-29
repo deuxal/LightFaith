502be07eefaa99921983c9cb252cc92b174ba751
deuxal <103450401+deuxal@users.noreply.github.com>
2023-06-28 21:38:01 -0400

    Rain assets
    Ayuda al profe
    -El sistema de cajon
    -EL animator del climb

--- Assets/Scripts/PlayerSpriteController.cs
+++ Assets/Scripts/PlayerSpriteController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerSpriteController : MonoBehaviour
 {
     public Sprite[] sprites; // Array of sprite images for different directions
-    public Transform crosshair; // Reference to the crosshair object
 
     private SpriteRenderer spriteRenderer;
 
@@ -16,44 +15,68 @@ public class PlayerSpriteController : MonoBehaviour
 
     private void Update()
     {
-        if (crosshair != null)
-        {
-            Vector3 direction = crosshair.position - transform.position;
-            direction.z = 0f;
-            if (direction.magnitude > 0.01f)
-            {
-                // Calculate the angle between the direction and the up vector
-                float angle = Vector3.SignedAngle(Vector3.up, direction.normalized, Vector3.forward);
+        // Get the cursor position in screen coordinates
+        Vector3 cursorPosition = Input.mousePosition;
 
-                // Determine the index of the sprite image based on the angle
-                int spriteIndex = GetSpriteIndex(angle);
+        // Convert the cursor position to world coordinates
+        Vector3 worldCursorPosition = Camera.main.ScreenToWorldPoint(cursorPosition);
+        worldCursorPosition.z = 0f;
 
-                // Set the sprite image
-                spriteRenderer.sprite = sprites[spriteIndex];
-            }
-        }
+        // Calculate the direction from the player to the cursor position
+        Vector3 direction = worldCursorPosition - transform.position;
+
+        // Calculate the angle between the direction and the up vector
+        float angle = Vector3.SignedAngle(Vector3.up, direction.normalized, Vector3.forward);
+
+        // Determine the index of the sprite image based on the angle
+        int spriteIndex = GetSpriteIndex(angle);
+
+        // Set the sprite image
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 
     private int GetSpriteIndex(float angle)
     {
         // Calculate the index of the sprite image based on the angle range
         // Adjust the angles and sprite indices according to your specific sprite setup
-        if (angle >= -45f && angle < 45f)
+
+        // Angles for each direction
+        float bottomLeftAngle = -135f;
+        float leftAngle = -90f;
+        float upperLeftAngle = -45f;
+        float upAngle = 0f;
+        float rightAngle = 90f;
+        float upperRightAngle = 45f;
+        float bottomRightAngle = 135f;
+
+        // Determine the index based on angle comparison
+        if (angle >= bottomLeftAngle && angle < leftAngle)
         {
-            return 0; // Sprite index for right direction
+            return 0; // Sprite index for bottom left direction
         }
-        else if (angle >= 45f && angle < 135f)
+        else if (angle >= leftAngle && angle < upperLeftAngle)
         {
-            return 1; // Sprite index for up direction
+            return 1; // Sprite index for left direction
         }
-        else if (angle >= -135f && angle < -45f)
+        else if (angle >= upperLeftAngle && angle < upAngle)
         {
-            return 3; // Sprite index for down direction
+            return 2; // Sprite index for upper left direction
+        }
+        else if (angle >= upAngle && angle < rightAngle)
+        {
+            return 3; // Sprite index for up direction
+        }
+        else if (angle >= rightAngle && angle < upperRightAngle)
+        {
+            return 4; // Sprite index for right direction
+        }
+        else if (angle >= upperRightAngle && angle < bottomRightAngle)
+        {
+            return 5; // Sprite index for upper right direction
         }
         else
         {
-            return 2; // Sprite index for left direction
+            return 6; // Sprite index for bottom right direction
         }
     }
 }
-
