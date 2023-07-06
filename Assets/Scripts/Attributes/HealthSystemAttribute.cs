a0465e8cb68e31f98ae9921a5b6cc21537326d36
deuxal <103450401+deuxal@users.noreply.github.com>
2023-07-05 20:51:11 -0400

    Health and health ui
    Preguntarle al prfoe sobre el cajon y el animator del climb y el walk

--- Assets/Scripts/Attributes/HealthSystemAttribute.cs
+++ Assets/Scripts/Attributes/HealthSystemAttribute.cs
@@ -1,73 +1,66 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 [AddComponentMenu("Playground/Attributes/Health System")]
 public class HealthSystemAttribute : MonoBehaviour
 {
-	public int health = 3;
+    public int health = 3;
 
-	private UIScript ui;
-	private int maxHealth;
+    private UIScript ui;
+    private int maxHealth;
+    private int playerNumber;
 
-	// Will be set to 0 or 1 depending on how the GameObject is tagged
-	// it's -1 if the object is not a player
-	private int playerNumber;
+    public event Action<int> OnHealthModified; // Event to notify when health is modified
 
+    private void Start()
+    {
+        ui = GameObject.FindObjectOfType<UIScript>();
 
+        switch (gameObject.tag)
+        {
+            case "Player":
+                playerNumber = 0;
+                break;
+            case "Player2":
+                playerNumber = 1;
+                break;
+            default:
+                playerNumber = -1;
+                break;
+        }
 
-	private void Start()
-	{
-		// Find the UI in the scene and store a reference for later use
-		ui = GameObject.FindObjectOfType<UIScript>();
+        if (ui != null && playerNumber != -1)
+        {
+            ui.SetHealth(health, playerNumber);
+        }
 
-		// Set the player number based on the GameObject tag
-		switch(gameObject.tag)
-		{
-			case "Player":
-				playerNumber = 0;
-				break;
-			case "Player2":
-				playerNumber = 1;
-				break;
-			default:
-				playerNumber = -1;
-				break;
-		}
+        maxHealth = health;
+    }
 
-		// Notify the UI so it will show the right initial amount
-		if(ui != null
-			&& playerNumber != -1)
-		{
-			ui.SetHealth(health, playerNumber);
-		}
+    public int GetCurrentHealth()
+    {
+        return health;
+    }
 
-		maxHealth = health; //note down the maximum health to avoid going over it when the player gets healed
-	}
+    public void ModifyHealth(int amount)
+    {
+        if (health + amount > maxHealth)
+        {
+            amount = maxHealth - health;
+        }
 
+        health += amount;
 
-	// changes the energy from the player
-	// also notifies the UI (if present)
-	public void ModifyHealth(int amount)
-	{
-		//avoid going over the maximum health by forcin
-		if(health + amount > maxHealth)
-		{
-			amount = maxHealth - health;
-		}
-			
-		health += amount;
+        if (ui != null && playerNumber != -1)
+        {
+            ui.ChangeHealth(amount, playerNumber);
+        }
 
-		// Notify the UI so it will change the number in the corner
-		if(ui != null
-			&& playerNumber != -1)
-		{
-			ui.ChangeHealth(amount, playerNumber);
-		}
+        OnHealthModified?.Invoke(health); // Invoke the OnHealthModified event with the updated health value
 
-		//DEAD
-		if(health <= 0)
-		{
-			Destroy(gameObject);
-		}
-	}
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image[] healthBars; // An array of health bars in the HealthUI image
+    public HealthSystemAttribute healthSystem; // Reference to the HealthSystemAttribute script or component
+
+    private int maxHealthBars;
+
+    private void Start()
+    {
+        maxHealthBars = healthBars.Length;
+        UpdateHealthBars();
+        healthSystem.OnHealthModified += OnHealthModified;
+    }
+
+    private void OnHealthModified(int currentHealth)
+    {
+        UpdateHealthBars();
+    }
+
+    private void UpdateHealthBars()
+    {
+        int currentHealth = healthSystem.GetCurrentHealth();
+
+        // Calculate the number of active health bars based on the current health
+        int activeHealthBars = Mathf.CeilToInt((float)currentHealth / maxHealthBars);
+
+        // Activate/deactivate bars based on the activeHealthBars count
+        for (int i = 0; i < healthBars.Length; i++)
+        {
+            healthBars[i].gameObject.SetActive(i < activeHealthBars);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        healthSystem.OnHealthModified -= OnHealthModified;
+    }
+}
