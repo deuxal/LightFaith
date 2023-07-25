e80e8df3cc090d158a46d0a389a8b0c5f7589650
deuxal <103450401+deuxal@users.noreply.github.com>
2023-07-24 23:17:11 -0400

    Fade (Level1) and flashlight sound added

--- Assets/Scripts/MouseControlledLight.cs
+++ Assets/Scripts/MouseControlledLight.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
+[RequireComponent(typeof(AudioSource))] // Ensure that an AudioSource component is attached to the GameObject
 public class MouseControlledLight : MonoBehaviour
 {
     public Light2D globalLight2D;
@@ -10,16 +11,25 @@ public class MouseControlledLight : MonoBehaviour
     public KeyCode modeChangeKey = KeyCode.Space;
     public float batteryDuration = 10f; // Duration of the battery in seconds
 
+    public AudioClip flashlightSoundClip; // Audio clip for flashlight activation
+
     private bool isLightOn = true;
     private bool isProjectileMode = false;
     private float batteryTimer;
-    public float originalBatteryDuration;
+    private float originalBatteryDuration;
 
+    private AudioSource audioSource; // Reference to the AudioSource component
     private ObjectMovement playerMovementController; // Reference to the ObjectMovement script
 
     private void Start()
     {
         playerMovementController = FindObjectOfType<ObjectMovement>(); // Find the ObjectMovement script in the scene
+
+        // Add an AudioSource component if not already present
+        if (!TryGetComponent(out audioSource))
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
 
         // Deactivate the flashlight at the start of the game
         globalLight2D.enabled = false;
@@ -36,6 +46,12 @@ public class MouseControlledLight : MonoBehaviour
         {
             isLightOn = !isLightOn;
             globalLight2D.enabled = isLightOn;
+
+            // Play the flashlight activation sound when the flashlight is turned on
+            if (isLightOn && flashlightSoundClip != null)
+            {
+                audioSource.PlayOneShot(flashlightSoundClip);
+            }
         }
 
         // Change mode when the specified key is held down
@@ -88,17 +104,17 @@ public class MouseControlledLight : MonoBehaviour
         }
     }
 
-
     public void ResetBatteryDuration()
     {
         batteryDuration = originalBatteryDuration;
         batteryTimer = batteryDuration;
     }
+
     public float BatteryPercentage()
     {
         return batteryTimer / batteryDuration;
     }
-
 }
+
 
 
